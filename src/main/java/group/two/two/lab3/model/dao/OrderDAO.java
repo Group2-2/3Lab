fd27128ad3c0f98296571f500a6398fed27ac57b
Oljk <oljk2512@gmail.com>
2018-07-17 23:47:27 +0300

    for order

--- src/main/java/group/two/two/lab3/model/dao/OrderDAO.java
+++ src/main/java/group/two/two/lab3/model/dao/OrderDAO.java
@@ -1,4 +1,13 @@
 package group.two.two.lab3.model.dao;
 
+import group.two.two.lab3.model.entities.Book;
+import group.two.two.lab3.model.entities.Order;
+
 public interface OrderDAO extends DAO {
+    boolean addBook(int order_id, int book_id);
+    boolean deleteBook(int order_id, int book_id);
+    boolean makeOrderDone(int order_id);
+    boolean addBook(Order order, Book book);
+    boolean deleteBook(Order order, Book book);
+    boolean makeOrderDone(Order order);
 }
