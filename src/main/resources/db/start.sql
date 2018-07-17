17df20d988b2ec7ace140e9f6ae6fb504c0d4c91
Oljk <oljk2512@gmail.com>
2018-07-17 23:51:19 +0300

    adding user and admin

---
+++ src/main/resources/db/start.sql
@@ -0,0 +1,9 @@
+insert into users (user_id, name, surname, e_mail, password, login, phone_number, ISADMIN)
+values (1, 'Admin', 'Adminovskiy', 'admin@gmail.com', '1234', 'admin', '+3805566113', 1);
+
+insert into users (user_id, name, surname, e_mail, password, login, phone_number, ISADMIN)
+values (2, 'User1', 'User1Sur', 'user1@gmail.com', '1234', 'user1', '+380556f6113', null);
+
+insert into users (user_id, name, surname, e_mail, password, login, phone_number, ISADMIN)
+values (3, 'User2', 'User2Sur', 'user2@gmail.com', '1234', 'user2', '+380556f6113', null);
+
